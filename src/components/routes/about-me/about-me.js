a45d9d6c8c2aa82618a8abf61e5a66a850726fe5
Charles Stover <github@charlesstover.com>
2018-08-05 23:04:42 -0500

    react-quotes

--- src/components/routes/about-me/about-me.js
+++ src/components/routes/about-me/about-me.js
@@ -1,6 +1,7 @@
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
+import Quotes from 'react-quotes';
 import withStyles from './about-me-styles';
 
 const backEndYears = new Date().getFullYear() - 2006;
@@ -9,23 +10,28 @@ const frontEndYears = new Date().getFullYear() - 2001;
 class AboutMe extends React.PureComponent {
   render() {
     return (
-      <Paper className={this.props.classes.paper}>
-        <Typography variant="headline">Coding Background</Typography>
-        <Typography paragraph>
-          My name is Charles Stover.
-          I am a senior web development consultant currently employed with a focus in React and Node.
-          I have over 15 years experience in web development languages such as JavaScript and PHP.
-          My focus thus far has been in automated content generation, micro-optimization, SEO, and web security.
-          All of my work strives for optimal UI/UX through modern design principles, optimized performance, and standardization.
-        </Typography>
-        <Typography paragraph>
-          I've been developing front end applications for {frontEndYears} years and server-side apps in PHP and MySQL for {backEndYears}.
-          During this time, I have released countless tools to the public and published multiple web development tutorials.
-        </Typography>
-        <Typography paragraph>
-          This website is merely a hub for my portfolio and e-mail as well as a demonstration of the art form known as optimization.
-        </Typography>
-      </Paper>
+      <React.Fragment>
+        <Paper className={this.props.classes.paper}>
+          <Quotes />
+        </Paper>
+        <Paper className={this.props.classes.paper}>
+          <Typography variant="headline">Coding Background</Typography>
+          <Typography paragraph>
+            My name is Charles Stover.
+            I am a senior web development consultant currently employed with a focus in React and Node.
+            I have over 15 years experience in web development languages such as JavaScript and PHP.
+            My focus thus far has been in automated content generation, micro-optimization, SEO, and web security.
+            All of my work strives for optimal UI/UX through modern design principles, optimized performance, and standardization.
+          </Typography>
+          <Typography paragraph>
+            I've been developing front end applications for {frontEndYears} years and server-side apps in PHP and MySQL for {backEndYears}.
+            During this time, I have released countless tools to the public and published multiple web development tutorials.
+          </Typography>
+          <Typography paragraph>
+            This website is merely a hub for my portfolio and e-mail as well as a demonstration of the art form known as optimization.
+          </Typography>
+        </Paper>
+      </React.Fragment> 
     );
   }
 }
