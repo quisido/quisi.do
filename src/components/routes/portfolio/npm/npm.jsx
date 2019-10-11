dfac1943d8c18120122a0166be84602087b8367c
Charles Stover <github@charlesstover.com>
2019-10-11 15:59:57 -0500

    separate npm api from display npm

--- src/components/routes/portfolio/npm/npm.jsx
+++ src/components/routes/portfolio/npm/npm.jsx
@@ -1,13 +1,13 @@
 import { List, Paper, Typography } from '@material-ui/core';
 import React from 'react';
-import npm from '../../../../assets/npm.json';
+import npm from '../../../../assets/npm';
 import Link from './link';
 import npmDownloads from './npm-downloads';
 import withStyles from './npm-styles';
 import sortPackagesByDownloads from './utils/sort-packages-by-downloads';
 
 // Deep clone the NPM packages.
-const npmPackages = npm.packages.map(pkg => [...pkg]);
+const npmPackages = npm.map(pkg => [...pkg]);
 
 class Npm extends React.PureComponent {
 
