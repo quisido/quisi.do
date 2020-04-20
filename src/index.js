e61833f4f8bd2cc4c4fb977967427f8d4b02120f
Charles Stover <github@charlesstover.com>
2020-04-19 19:07:24 -0700

    adds logrocket

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import { unregister } from './register-service-worker';
+
+LogRocket.init('hth2me/charlesstovercom');
+setupLogRocketReact(LogRocket);
 
 ReactDOM.render(
   <App />,
