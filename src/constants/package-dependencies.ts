4253478a3ea9ddf20f64e4b619301b15fd465727
Charles Stover <github@charlesstover.com>
2021-01-01 17:03:46 -0800

    package list

--- src/constants/package-dependencies.ts
+++ src/constants/package-dependencies.ts
@@ -1,4 +1,5 @@
 const NPM_PACKAGE_DEPENDENCIES: Map<string, string[]> = new Map([
+  ['awsui-dark-mode', ['awsui-theme']],
   ['fetch-suspense', ['deep-equal']],
   ['reactn', ['use-force-update']],
   ['use-react-router', ['use-force-update']],
