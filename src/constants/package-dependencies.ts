482ff9d3d52edd8c1b1152f5b1817019c2e9ed19
Charles Stover <github@charlesstover.com>
2021-01-27 21:58:15 -0800

    update packages

--- src/constants/package-dependencies.ts
+++ src/constants/package-dependencies.ts
@@ -1,6 +1,10 @@
 const NPM_PACKAGE_DEPENDENCIES: Map<string, string[]> = new Map([
   ['awsui-dark-mode', ['awsui-theme']],
   ['fetch-suspense', ['deep-equal']],
+  ['rainbow-gradient', ['@charlesstover/hsl2rgb']],
+  ['react-capsule', ['use-force-update']],
+  ['react-rainbow-text', ['@charlesstover/hsl2rgb', 'rainbow-gradient']],
+  ['react-sparkline-svg', ['sparkline-svg']],
   ['reactn', ['use-force-update']],
   ['use-react-router', ['use-force-update']],
 ]);
