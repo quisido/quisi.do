309e7c6410d65a3423ea5e2bf2e72c590223bc4e
Charles Stover <chastove@amazon.com>
2021-04-28 22:42:55 -0700

    sentry

--- src/components/app/app.view.tsx
+++ src/components/app/app.view.tsx
@@ -1,9 +1,11 @@
+import { ErrorBoundary, withProfiler } from '@sentry/react';
 import AwsuiDarkMode from 'awsui-dark-mode';
 import { I18nProvider } from 'lazy-i18n';
 import { ReactElement } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router';
 import Routes from '../../components/routes';
+import history from '../../constants/history';
 import Language from '../../constants/language';
 import TRANSLATIONS from '../../constants/translations';
 import useDarkMode from '../../hooks/use-dark-mode';
@@ -11,23 +13,27 @@ import useLanguage from '../../hooks/use-language';
 
 const queryClient: QueryClient = new QueryClient();
 
-export default function App(): ReactElement {
+function App(): ReactElement {
   const [isDarkModeEnabled] = useDarkMode();
   const [language] = useLanguage();
 
   return (
-    <AwsuiDarkMode disabled={!isDarkModeEnabled} root="body">
-      <BrowserRouter>
+    <ErrorBoundary fallback="An error occurred.">
+      <AwsuiDarkMode disabled={!isDarkModeEnabled} root="body">
         <I18nProvider
           fallbackLocale={Language.English}
           locale={language}
           translations={TRANSLATIONS}
         >
           <QueryClientProvider client={queryClient}>
-            <Routes />
+            <Router history={history}>
+              <Routes />
+            </Router>
           </QueryClientProvider>
         </I18nProvider>
-      </BrowserRouter>
-    </AwsuiDarkMode>
+      </AwsuiDarkMode>
+    </ErrorBoundary>
   );
 }
+
+export default withProfiler(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,24 @@
 import '@awsui/global-styles/index.css';
+import { init, reactRouterV5Instrumentation } from '@sentry/react';
+import { RouterHistory } from '@sentry/react/dist/reactrouter';
+import { Integrations } from '@sentry/tracing';
 import { render } from 'react-dom';
 import App from './components/app';
+import history from './constants/history';
+
+const SENTRY_KEY = 'a36b53fdd093405eb597a945f49a70f2';
+
+const browserTracing = new Integrations.BrowserTracing({
+  routingInstrumentation: reactRouterV5Instrumentation(
+    (history as unknown) as RouterHistory,
+  ),
+});
+
+init({
+  dsn: `https://${SENTRY_KEY}@o592283.ingest.sentry.io/5740642`,
+  release: 'charlesstover.com@' + process.env.npm_package_version,
+  tracesSampleRate: 1.0,
+  integrations: [browserTracing],
+});
 
 render(<App />, document.getElementById('root'));
