ef14668a89f2ad6b3d8cd30273fb19d0e8108975
Charles Stover <github@charlesstover.com>
2021-08-16 09:37:10 -0700

    use errorBoundaryFallback

--- src/features/app/app.view.tsx
+++ src/features/app/app.view.tsx
@@ -8,11 +8,28 @@ import HISTORY from '../../constants/history';
 import STORE from '../../constants/redux-store';
 import Main from './app.view.main';
 
+interface FallbackRenderParams {
+  readonly componentStack: string | null;
+  readonly error: Readonly<Error>;
+  readonly eventId: string | null;
+  readonly resetError: () => void;
+}
+
 const queryClient: QueryClient = new QueryClient();
+
+const errorBoundaryFallback = ({
+  error,
+  resetError,
+}: FallbackRenderParams): ReactElement => (
+  <>
+    <strong>An error occurred while rendering the application:</strong>
+    <span>{error.message}</span> <button onClick={resetError}>Retry</button>
+  </>
+);
 
 function App(): ReactElement {
   return (
-    <ErrorBoundary fallback={<>An error occurred.</>}>
+    <ErrorBoundary fallback={errorBoundaryFallback}>
       <Provider store={STORE}>
         <QueryClientProvider client={queryClient}>
           <Router history={HISTORY}>
