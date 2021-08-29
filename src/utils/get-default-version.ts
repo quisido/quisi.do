05255cb0888bbd8aa443e379e2a518caa8327e80
Charles Stover <github@charlesstover.com>
2021-08-29 15:22:02 -0700

    set version to UTC

--- src/utils/get-default-version.ts
+++ src/utils/get-default-version.ts
@@ -3,11 +3,11 @@ import leftPad from '../utils/left-pad';
 const DATE: Date = new Date();
 const MONTH_OFFSET = 1;
 
-const DAY: string = leftPad(DATE.getDate());
-const HOURS: string = leftPad(DATE.getHours());
-const MINUTES: string = leftPad(DATE.getMinutes());
-const MONTH: string = leftPad(DATE.getMonth() + MONTH_OFFSET);
-const YEAR: number = DATE.getFullYear();
+const DAY: string = leftPad(DATE.getUTCDate());
+const HOURS: string = leftPad(DATE.getUTCHours());
+const MINUTES: string = leftPad(DATE.getUTCMinutes());
+const MONTH: string = leftPad(DATE.getUTCMonth() + MONTH_OFFSET);
+const YEAR: number = DATE.getUTCFullYear();
 
 export default function getDefaultVersion(): string {
   return `${YEAR}.${MONTH}${DAY}.${HOURS}${MINUTES}`;
