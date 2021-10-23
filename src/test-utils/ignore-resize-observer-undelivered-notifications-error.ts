c26f2987bd2cdb5100bd3bb0f58d10d9b046bb42
Charles Stover <github@charlesstover.com>
2021-10-22 23:33:24 -0700

    use regex to test error message

--- src/test-utils/ignore-resize-observer-undelivered-notifications-error.ts
+++ src/test-utils/ignore-resize-observer-undelivered-notifications-error.ts
@@ -1,7 +1,20 @@
 /// <reference types="cypress" />
 
+const filterErrorByResizeObserverUndeliveredNotificationsError = (
+  err: Readonly<Error>,
+): boolean =>
+  err.message.includes(
+    'ResizeObserver loop completed with undelivered notifications.',
+  );
+
+const inverse =
+  <T>(f: (value: T) => boolean): ((value: T) => boolean) =>
+  (value: T): boolean =>
+    !f(value);
+
 export default function ignoreResizeObserverUndeliveredNotificationsError(): void {
-  cy.on('uncaught:exception', (err: Readonly<Error>): boolean => {
-    throw new Error(`{${JSON.stringify(err.message)}}`);
-  });
+  cy.on(
+    'uncaught:exception',
+    inverse(filterErrorByResizeObserverUndeliveredNotificationsError),
+  );
 }
