9119a42f9f1901878020d8a13920c3df3790b77a
Charles Stover <github@charlesstover.com>
2021-10-22 23:05:53 -0700

    shape error message

--- src/test-utils/ignore-resize-observer-undelivered-notifications-error.ts
+++ src/test-utils/ignore-resize-observer-undelivered-notifications-error.ts
@@ -2,13 +2,6 @@
 
 export default function ignoreResizeObserverUndeliveredNotificationsError(): void {
   cy.on('uncaught:exception', (err: Readonly<Error>): boolean => {
-    if (
-      err.message ===
-      'ResizeObserver loop completed with undelivered notifications.'
-    ) {
-      return false;
-    }
-
-    return true;
+    throw new Error(`{${JSON.stringify(err.message)}}`);
   });
 }
