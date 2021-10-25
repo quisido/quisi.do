bfece45aa912621e65129680ef79c9b81c5d2d3c
Charles Stover <github@charlesstover.com>
2021-10-24 17:10:33 -0700

    set default mui theme options to dense

--- src/components/theme/constants/mui-dark-theme.ts
+++ src/components/theme/constants/mui-dark-theme.ts
@@ -1,7 +1,9 @@
 import type { Theme } from '@mui/material/styles';
 import { createTheme } from '@mui/material/styles';
+import DEFAULT_THEME_OPTIONS from './default-mui-theme-options';
 
 const MUI_DARK_THEME: Theme = createTheme({
+  ...DEFAULT_THEME_OPTIONS,
   palette: {
     mode: 'dark',
   },
--- src/components/theme/constants/mui-light-theme.ts
+++ src/components/theme/constants/mui-light-theme.ts
@@ -1,7 +1,9 @@
 import type { Theme } from '@mui/material/styles';
 import { createTheme } from '@mui/material/styles';
+import DEFAULT_THEME_OPTIONS from './default-mui-theme-options';
 
 const MUI_LIGHT_THEME: Theme = createTheme({
+  ...DEFAULT_THEME_OPTIONS,
   palette: {
     mode: 'light',
   },
