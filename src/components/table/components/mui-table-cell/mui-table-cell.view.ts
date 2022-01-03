cee4f49fec4b9a4afc17581618520d2d2dbd2a04
Charles Stover <github@charlesstover.com>
2022-01-03 02:38:53 -0800

    fix sorting header color

--- src/components/table/components/mui-table-cell/mui-table-cell.view.ts
+++ src/components/table/components/mui-table-cell/mui-table-cell.view.ts
@@ -1,11 +1,28 @@
+import type { Palette } from '@mui/material/styles';
 import { styled } from '@mui/material/styles';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+const mapPaletteToBackgroundColor = ({ mode, primary }: Palette): string => {
+  if (mode === 'dark') {
+    return primary.dark;
+  }
+  return primary.light;
+};
+
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 const MuiTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
+    backgroundColor: mapPaletteToBackgroundColor(theme.palette),
     color: theme.palette.common.white,
+
+    '& .Mui-active, & .Mui-active svg': {
+      color: theme.palette.common.white,
+    },
+
+    '& .MuiTableSortLabel-root:hover, & .MuiTableSortLabel-root:hover svg': {
+      color: theme.palette.common.white,
+    },
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
