f0abd25afeadcb4d63bbb2f645909054dacc129e
Charles Stover <github@charlesstover.com>
2022-01-15 18:52:46 -0800

    fix packages in firefox

--- src/features/packages/components/contents/packages-contents.hook.tsx
+++ src/features/packages/components/contents/packages-contents.hook.tsx
@@ -9,27 +9,6 @@ import type Item from '../../types/packages-item';
 import filterDefaultPackage from '../../utils/filter-default-package';
 import mapNpmDownloadsEntryToItem from '../../utils/map-npm-downloads-entry-to-item';
 import useColumns from './packages-contents.hook.columns';
-// import type { CollectionPreferencesProps } from '@awsui/components-react/collection-preferences';
-// import type { NonCancelableCustomEvent } from '@awsui/components-react/interfaces';
-// import type { PaginationProps } from '@awsui/components-react/pagination';
-// import type { TableProps } from '@awsui/components-react/table';
-// import type { TextFilterProps } from '@awsui/components-react/text-filter';
-// import {
-//   useCollectionPreferences,
-//   usePagination,
-//   useTable,
-//   useTextFilter,
-// } from 'use-awsui';
-// import useAwsuiTableItemDescription from 'use-awsui-table-item-description';
-// import useNpmDownloads from '../../../../hooks/use-npm-downloads';
-// import type ReadonlyTableSortingEvent from '../../../../types/readonly-table-sorting-event';
-// import filterDefaultPackage from '../../utils/filter-default-package';
-// import mapNpmDownloadsEntryToItem from '../../utils/map-npm-downloads-entry-to-item';
-// import useColumnDefinitions from './packages-contents.hook.column-definitions';
-// import useCountText from './packages-contents.hook.count-text';
-// import usePageSizePreference from './packages-contents.hook.page-size-preference';
-// import useVisibleContentPreference from './packages-contents.hook.visible-content-preference';
-// import useWrapLinesPreference from './packages-contents.hook.wrap-lines-preference';
 
 interface State {
   readonly columns: readonly TableColumn<Item>[];
