239004ed1f8e5aab567a7dc8110da5af89778a1e
Charles Stover <github@charlesstover.com>
2022-01-20 11:08:02 -0800

    remove unused interface

--- src/features/app/types/app-state.ts
+++
@@ -1,8 +0,0 @@
-import type DesignSystem from '../../../constants/design-system';
-import type Language from '../../../constants/language';
-
-export default interface AppState {
-  readonly designSystem: DesignSystem;
-  readonly isDarkModeEnabled: boolean;
-  readonly language: Language;
-}
