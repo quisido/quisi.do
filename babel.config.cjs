7db59a6ae5a877e1a76ea33edf5f2433a34fd82c
Charles Stover <github@charlesstover.com>
2022-01-21 15:36:36 -0800

    add istanbul to babel

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,5 +1,6 @@
 // Don't process node_modules except for AWS UI.
 module.exports = {
   ignore: [/node_modules\/?!@awsui\/components-react/],
+  plugins: ['istanbul'],
   presets: ['@babel/preset-env'],
 };
