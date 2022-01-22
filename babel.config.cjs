33c3b1db996e93347fa1c6fe1ed1fd36c306b754
Charles Stover <github@charlesstover.com>
2022-01-21 18:15:23 -0800

    rm istanbul from babel

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,5 @@
 // Don't process node_modules except for AWS UI.
 module.exports = {
   ignore: [/node_modules\/?!@awsui\/components-react/],
-  plugins: ['istanbul'],
   presets: ['@babel/preset-env'],
 };
