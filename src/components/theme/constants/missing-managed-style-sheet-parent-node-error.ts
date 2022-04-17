5f16bfa2ec2a78edebeb1e4be7e99b4009f5d882
Charles Stover <github@charlesstover.com>
2022-04-16 20:33:12 -0700

    fail silently when required style sheet is missing (#90)

---
+++ src/components/theme/constants/missing-managed-style-sheet-parent-node-error.ts
@@ -0,0 +1,5 @@
+const MISSING_MANAGED_STYLE_SHEET_PARENT_NODE_ERROR = new Error(
+  'Expected managed style sheet to have a parent node.',
+);
+
+export default MISSING_MANAGED_STYLE_SHEET_PARENT_NODE_ERROR;
--- src/components/theme/utils/get-awsui-global-style-element.ts
+++
@@ -1,8 +0,0 @@
-import '@awsui/global-styles/index.css';
-import mapStyleSheetListToAwsuiGlobalStyleElement from '../utils/map-style-sheet-list-to-awsui-global-style-element';
-
-export default function getAwsuiGlobalStyleElement():
-  | Element
-  | ProcessingInstruction {
-  return mapStyleSheetListToAwsuiGlobalStyleElement(document.styleSheets);
-}
