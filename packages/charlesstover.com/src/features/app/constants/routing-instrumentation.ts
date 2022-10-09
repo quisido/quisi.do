775b93973d342a09c6c63687c72f62a7a23a90d3
Charles Stover <github@charlesstover.com>
2022-10-09 03:17:49 -0700

    `yarn up` (#98)

--- packages/charlesstover.com/src/features/app/constants/routing-instrumentation.ts
+++ packages/charlesstover.com/src/features/app/constants/routing-instrumentation.ts
@@ -1,4 +1,6 @@
+import type { Location } from '@sentry/react/types/types';
 import { reactRouterV6Instrumentation } from '@sentry/react';
+import type { ReactElement, ReactNode } from 'react';
 import { useEffect } from 'react';
 import {
   createRoutesFromChildren,
@@ -7,12 +9,36 @@ import {
   useNavigationType,
 } from 'react-router-dom';
 
+// The following types are incorrectly defined and required by `@sentry/react`.
+// eslint-disable-next-line @typescript-eslint/no-type-alias
+type Params<Key extends string = string> = {
+  readonly [key in Key]: string | undefined;
+};
+
+interface RouteMatch<ParamKey extends string = string> {
+  params: Params<ParamKey>;
+  pathname: string;
+  pathnameBase: string;
+  route: RouteObject;
+}
+
+interface RouteObject {
+  caseSensitive?: boolean;
+  children?: RouteObject[];
+  element?: ReactNode;
+  index?: boolean;
+  path?: string;
+}
+
 const ROUTING_INSTRUMENTATION = reactRouterV6Instrumentation(
   useEffect,
   useLocation,
   useNavigationType,
-  createRoutesFromChildren,
-  matchRoutes,
+  createRoutesFromChildren as (children: ReactElement[]) => RouteObject[],
+  matchRoutes as (
+    routes: RouteObject[],
+    location: Location,
+  ) => RouteMatch[] | null,
 );
 
 export default ROUTING_INSTRUMENTATION;
