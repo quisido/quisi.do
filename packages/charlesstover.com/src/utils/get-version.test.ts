c70d37a4e8be601925fe3ac97545e0cf62ab2bc2
Charles Stover <github@charlesstover.com>
2022-11-05 16:11:20 -0700

    add Lighthouse (#103)

--- packages/charlesstover.com/src/utils/get-version.test.ts
+++ packages/charlesstover.com/src/utils/get-version.test.ts
@@ -1,21 +1,40 @@
 import getVersion from './get-version';
 
+const setGitHubSha = (value: string): void => {
+  process.env.REACT_APP_GITHUB_SHA = value;
+};
+
+const setNodeEnv = (value: string): void => {
+  Object.assign(process.env, {
+    NODE_ENV: value,
+  });
+};
+
 describe('getVersion', (): void => {
   afterEach((): void => {
+    setNodeEnv('test');
     delete process.env.REACT_APP_GITHUB_SHA;
   });
 
-  it('should return the REACT_APP_GITHUB_SHA environment variable', (): void => {
-    process.env.REACT_APP_GITHUB_SHA =
-      '000f3f5e6cc58f5106e5deb9e8caf3be1e1727fe';
+  it('should be the GitHub SHA', (): void => {
+    setGitHubSha('000f3f5e6cc58f5106e5deb9e8caf3be1e1727fe');
     expect(getVersion()).toBe('9at3u3bqugc');
 
-    process.env.REACT_APP_GITHUB_SHA =
-      '135314872f0151ba911c888df082b9adfe375e11';
+    setGitHubSha('135314872f0151ba911c888df082b9adfe375e11');
     expect(getVersion()).toBe('30mn721728i');
   });
 
-  it('should be `alpha` in development mode', (): void => {
+  it('should be "gamma" in production builds', (): void => {
+    setNodeEnv('production');
+    expect(getVersion()).toBe('gamma');
+  });
+
+  it('should be "alpha" in development builds', (): void => {
+    setNodeEnv('development');
     expect(getVersion()).toBe('alpha');
+  });
+
+  it('should default to "beta"', (): void => {
+    expect(getVersion()).toBe('beta');
   });
 });
