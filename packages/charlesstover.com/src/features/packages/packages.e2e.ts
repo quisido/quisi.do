8d4f40c9b0d16df701ba25c4aa51734e2f5d18e6
Charles Stover <github@charlesstover.com>
2022-11-10 12:01:40 -0800

    Update packages.e2e.ts

--- packages/charlesstover.com/src/features/packages/packages.e2e.ts
+++ packages/charlesstover.com/src/features/packages/packages.e2e.ts
@@ -2,9 +2,10 @@
 
 import setDesignSystem from '../../test/cypress/utils/set-design-system';
 
+const DESIGN_SYSTEMS: string[] = ['AWS', 'Cloudscape', 'Material'];
+const NEXT_PAGE_SELECTOR = 'button[aria-label="Go to next page"]';
 const FIRST_PACKAGE_SELECTOR =
   'main table > tbody > tr:first-child > td:first-child a';
-const DESIGN_SYSTEMS: string[] = ['AWS', 'Cloudscape', 'Material'];
 
 const shouldDisplayCriticalElements = (): void => {
   cy.contains('nav', 'Packages');
@@ -13,13 +14,13 @@ const shouldDisplayCriticalElements = (): void => {
 
 const shouldPaginate = (): void => {
   cy.get(FIRST_PACKAGE_SELECTOR)
+    .last()
     .invoke('text')
     .then((page1package1name: string): void => {
-      cy.get('button[aria-label="Go to next page"]')
+      cy.get(NEXT_PAGE_SELECTOR).last().scrollIntoView().click();
+      cy.get(FIRST_PACKAGE_SELECTOR)
         .last()
-        .scrollIntoView()
-        .click();
-      cy.get(FIRST_PACKAGE_SELECTOR).should('not.contain', page1package1name);
+        .should('not.contain', page1package1name);
     });
 };
 
