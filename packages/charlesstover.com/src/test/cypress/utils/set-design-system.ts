62440b1b59911530db27037610e6d3dc7c5ba75a
Charles Stover <github@charlesstover.com>
2022-11-11 10:44:25 -0800

    Update set-design-system.ts

--- packages/charlesstover.com/src/test/cypress/utils/set-design-system.ts
+++ packages/charlesstover.com/src/test/cypress/utils/set-design-system.ts
@@ -2,6 +2,7 @@ import select from './select';
 
 const EXPANDO_SELECTOR = '*[role="button"]';
 const NAVIGATION_ITEM_SELECTOR = 'nav ul > li';
+const UNMOUNT_TIMEOUT = 200;
 
 export default function setDesignSystem(designSystem: string): void {
   beforeEach((): void => {
@@ -12,9 +13,15 @@ export default function setDesignSystem(designSystem: string): void {
     });
 
     // Wait for the previous design system to unmount.
-    cy.get('body').should(($body: JQuery<HTMLBodyElement>): void => {
-      expect($body.text()).not.to.contain('CharlesStover.com');
-    });
+    try {
+      cy.get('body', { timeout: UNMOUNT_TIMEOUT }).should(
+        ($body: JQuery<HTMLBodyElement>): void => {
+          expect($body.text()).not.to.contain('CharlesStover.com');
+        },
+      );
+    } catch (_err: unknown) {
+      cy.log(`${designSystem} design system appears to be cached.`);
+    }
 
     // Wait for the new design system to mount.
     cy.get('body').should(($body: JQuery<HTMLBodyElement>): void => {
