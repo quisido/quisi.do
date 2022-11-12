16b4b4c0b648e505fe2aceb3946fe2cdac0e7317
Charles Stover <github@charlesstover.com>
2022-11-12 12:15:28 -0800

    change cypress config back to CJS

--- packages/charlesstover.com/cypress.config.cjs
+++ packages/charlesstover.com/cypress.config.cjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
   defaultCommandTimeout: 30000,
   fixturesFolder: false,
   projectId: 'fahz48',
