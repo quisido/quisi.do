f1ef04ab82fb695fb9e3ce85416e0e4e98f8b19a
Charles Stover <github@charlesstover.com>
2022-12-05 09:51:24 -0800

    merge ignored hosts

--- packages/charlesstover.com/cypress/support/constants/ignored-dependency-locations.ts
+++
@@ -1,8 +0,0 @@
-const IGNORED_DEPENDENCY_LOCATIONS: readonly string[] = [
-  'https://cloudflareinsights.com/',
-  'https://o592283.ingest.sentry.io/',
-  'https://rum.browser-intake-datadoghq.com/',
-  'https://session-replay.browser-intake-datadoghq.com/',
-];
-
-export default IGNORED_DEPENDENCY_LOCATIONS;
--- packages/charlesstover.com/cypress/support/e2e.ts
+++ packages/charlesstover.com/cypress/support/e2e.ts
@@ -1,10 +1,14 @@
 import '@cypress/code-coverage/support';
 import ignoreDependencyLogs from '@monorepo-template/ignore-cypress-dependency-logs';
-import IGNORED_DEPENDENCY_LOCATIONS from './constants/ignored-dependency-locations';
 import ignoreResizeObserverUndeliveredNotificationsError from '../../src/test/cypress/utils/ignore-resize-observer-undelivered-notifications-error';
 
 beforeEach(ignoreResizeObserverUndeliveredNotificationsError);
 
 beforeEach((): void => {
-  ignoreDependencyLogs(IGNORED_DEPENDENCY_LOCATIONS);
+  ignoreDependencyLogs([
+    'https://cloudflareinsights.com/',
+    'https://o592283.ingest.sentry.io/',
+    'https://rum.browser-intake-datadoghq.com/',
+    'https://session-replay.browser-intake-datadoghq.com/',
+  ]);
 });
