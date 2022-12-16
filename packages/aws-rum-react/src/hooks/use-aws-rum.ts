2e5bf3e7bd5570666a601f74fc6e16b27d8ca85d
Charles Stover <github@charlesstover.com>
2022-12-15 16:34:20 -0800

    add jest utility (#114)

--- packages/aws-rum-react/src/hooks/use-aws-rum.ts
+++ packages/aws-rum-react/src/hooks/use-aws-rum.ts
@@ -4,11 +4,11 @@ import MISSING_AWS_RUM_CONTEXT_ERROR from '../constants/missing-aws-rum-context-
 import AwsRumContext from '../contexts/aws-rum';
 
 export default function useAwsRum(): AwsRum {
-  const rum: AwsRum | null = useContext(AwsRumContext);
+  const client: AwsRum | null = useContext(AwsRumContext);
 
-  if (rum === null) {
+  if (client === null) {
     throw MISSING_AWS_RUM_CONTEXT_ERROR;
   }
 
-  return rum;
+  return client;
 }
