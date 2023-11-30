99302b6a56230257db8bf8cdc56e9b04f3ddf66f
quisi.do <github@quisi.do>
2023-11-30 00:29:31 -0800

    lighthouse stuff

--- packages/next/lighthouse.config.js
+++ packages/next/lighthouse.config.js
@@ -2,5 +2,13 @@ export default {
   extends: 'lighthouse:default',
   settings: {
     blockedUrlPatterns: ['https://cloudflareinsights.com/cdn-cgi/rum'],
+    budgetPath: 'lighthouse.budget.json',
+    chromeFlags: "--headless",
+    enableErrorReporting: true,
+    output: ["html", "json"],
+    outputPath: "lighthouse",
+    preset: "experimental",
+    saveAssets: true,
+    verbose: true,
   },
 };
