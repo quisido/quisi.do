ba095d85ecc5342e19f04d549532041c1f7f2436
Charles Stover <github@charlesstover.com>
2024-04-17 16:04:12 -0700

    deploy `whoami` in pipeline

--- packages/next/lighthouse.config.ts
+++ packages/next/lighthouse.config.ts
@@ -13,13 +13,20 @@ const lightghouseConfig: Config = {
     // saveAssets: true,
     // verbose: false,
     blockedUrlPatterns: [
-      'https://api.quisi.do/whoami', // temp. block for fixing commit to pass CI
+      'https://api.honeycomb.io/*',
+      'https://api.quisi.do/*', // temp block to allow fix to pass CI
       'https://browser-intake-datadoghq.com/*',
+      'https://challenges.cloudflare.com/*',
+      'https://clarity.ms/*',
       'https://cloudflareinsights.com/*',
+      'https://dataplane.rum.us-west-2.amazonaws.com/*',
       'https://edge.fullstory.com/*',
       'https://localhost:1098/whoami', // invalid certificate
       'https://o592283.ingest.sentry.io/*',
       'https://rs.fullstory.com/*',
+      'https://rum.browser-intake-datadoghq.com/*',
+      'https://session-replay.browser-intake-datadoghq.com/*',
+      'https://static.cloudflareinsights.com/*',
       'https://stats.g.doubleclick.net/*',
       'https://www.clarity.ms/*',
       'https://www.google.com/ads/*',
