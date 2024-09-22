3a010becf702f3732e3c8760476c05e07aa5efd6
quisi.do <github@quisi.do>
2024-09-21 19:04:31 -0700

    remove unnecessary `handleInvalidPatreonOAuthTokenResponse`

--- packages/authn/src/routes/patreon/map-patreon-oauth-token-to-access-token.ts
+++ packages/authn/src/routes/patreon/map-patreon-oauth-token-to-access-token.ts
@@ -1,16 +1,11 @@
 import isObject from '../../utils/is-object.js';
 import handleInvalidPatreonAccessToken from './handle-invalid-patreon-access-token.js';
-import handleInvalidPatreonOAuthTokenResponse from './handle-invalid-patreon-oauth-token-response.js';
 import handleInvalidPatreonOAuthToken from './handle-invalid-patreon-oauth-token.js';
 import handleMissingPatreonAccessToken from './handle-missing-patreon-access-token.js';
 
 export default function mapPatreonOAuthTokenToAccessToken(
   token: unknown,
 ): string {
-  if (typeof token === 'undefined') {
-    return handleInvalidPatreonOAuthTokenResponse();
-  }
-
   if (!isObject(token)) {
     return handleInvalidPatreonOAuthToken(token);
   }
