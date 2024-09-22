d61bf75580c3b79922d0b7daeb2bda9066e4c9fc
quisi.do <github@quisi.do>
2024-09-21 19:27:01 -0700

    weet woo

--- packages/authn/src/features/handle-fetch-error.ts
+++ packages/authn/src/features/handle-fetch-error.ts
@@ -18,7 +18,6 @@ export default function handleFetchError(
     return new ErrorResponse(err.cause, returnPath);
   }
 
-  console.log(err);
   emitPublicMetric({ name: MetricName.UnknownError });
   logPrivateError(mapUnknownToError(err));
   return new ErrorResponse(ErrorCode.Unknown, returnPath);
