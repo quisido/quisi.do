7e49714a8650ee7c594099396330cb19d9d0b918
quisi.do <github@quisi.do>
2024-10-19 21:31:01 -0700

    fix tests

---
+++ packages/next/src/components/rocket-loader-hotfix.tsx
@@ -0,0 +1,22 @@
+import type { ReactElement } from "react";
+
+const SCRIPT = `
+(function(w) {
+  w.addEventListener = function addEventListenerSpy(type, listener, options) {
+    if (type === 'unload') {
+      Window.prototype.addEventListener.call(w, 'pagehide', listener, options);
+      return;
+    }
+    Window.prototype.addEventListener.call(w, type, listener, options);
+  };
+})(window);
+`;
+
+export default function RocketLoaderHotfix(): ReactElement {
+  return (
+    <script
+      dangerouslySetInnerHTML={{ __html: SCRIPT }}
+      type="text/javascript"
+    />
+  );
+}
