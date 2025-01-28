23e0e95372395cf9d1c423e1687cc71b401af603
quisi.do <github@quisi.do>
2025-01-28 00:04:40 -0800

    Update map-configs-to-rules.ts

--- packages/eslint-config/src/map-configs-to-rules.ts
+++ packages/eslint-config/src/map-configs-to-rules.ts
@@ -1,6 +1,8 @@
-import type { Linter } from "eslint";
-import reduceConfigsToRules from "./reduce-configs-to-rules.js";
+import type { Linter } from 'eslint';
+import reduceConfigsToRules from './reduce-configs-to-rules.js';
 
-export default function mapConfigsToRules(configs: Linter.Config[]): Linter.RulesRecord {
+export default function mapConfigsToRules(
+  configs: Linter.Config[],
+): Linter.RulesRecord {
   return configs.reduce(reduceConfigsToRules, {});
 }
