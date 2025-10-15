fbb860b2de034ff6d208f1e4f705ddb955dff85f
quisi.do <github@quisi.do>
2025-10-14 22:38:48 -0700

    Delete sentry-fullstory-client.ts

--- packages/vite/src/types/sentry-fullstory-client.ts
+++
@@ -1,7 +0,0 @@
-import { type fullStoryIntegration } from '@sentry/fullstory';
-
-type Options = Parameters<typeof fullStoryIntegration>[1];
-
-type FullstoryClient = Options['client'];
-
-export type SentryFullstoryClient = FullstoryClient;
