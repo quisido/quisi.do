b5134ebfe39cb9b0be1bca0787cbef217fa6e222
quisi.do <github@quisi.do>
2025-11-06 03:04:57 -0800

    update vitest config

--- packages/vitest-config/src/coverage-options.ts
+++ packages/vitest-config/src/coverage-options.ts
@@ -3,11 +3,46 @@ import { EXCLUDE } from './exclude.js';
 import { THRESHOLDS } from './thresholds.js';
 
 export const COVERAGE_OPTIONS: CoverageOptions<'istanbul'> = {
-  all: true,
   clean: true,
   enabled: true,
   exclude: [...EXCLUDE],
   provider: 'istanbul',
+  reporter: [
+    ['clover', { file: 'clover.xml' }],
+    ['cobertura', { file: 'cobertura.xml' }],
+    ['json', { file: 'coverage.json' }],
+    ['json-summary', { file: 'coverage-summary.json' }],
+    ['lcov', { file: 'lcov.info' }],
+    ['lcovonly', { file: 'lcov-only.info' }],
+    [
+      'html',
+      {
+        skipEmpty: true,
+        verbose: true,
+      },
+    ],
+    [
+      'html-spa',
+      {
+        metricsToShow: ['branches', 'functions', 'lines', 'statements'],
+        skipEmpty: true,
+        verbose: true,
+      },
+    ],
+    ['teamcity', { file: 'teamcity.txt' }],
+    [
+      'text',
+      {
+        file: 'text.txt',
+        maxCols: 80,
+        skipEmpty: true,
+        skipFull: true,
+      },
+    ],
+    ['text-summary', { file: 'text-summary.txt' }],
+  ],
+  reportOnFailure: true,
+  reportsDirectory: '.vitest/coverage',
   skipFull: true,
   thresholds: THRESHOLDS,
 };
