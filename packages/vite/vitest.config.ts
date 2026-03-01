cb56e87f02bdb3f990847cf5476bad300caaf606
quisi.do <github@quisi.do>
2026-02-28 23:05:42 -0800

    fix build and vitest

--- packages/vite/vitest.config.ts
+++ packages/vite/vitest.config.ts
@@ -1,6 +1,8 @@
+import react from '@vitejs/plugin-react';
 import { defineVitestConfig, type VitestConfig } from 'quisi';
 
 const CONFIG: VitestConfig = await defineVitestConfig({
+  plugins: [react()],
   test: {
     coverage: {
       enabled: false,
