194519cef4f38eca6c6ca625060ef4a910f1c534
quisi.do <github@quisi.do>
2026-03-04 16:19:07 -0800

    Update create-compiler-options.ts

--- packages/quisido/src/features/start/create-compiler-options.ts
+++ packages/quisido/src/features/start/create-compiler-options.ts
@@ -17,7 +17,7 @@ export default async function createCompilerOptions(): Promise<server.protocol.C
      */
     rootDir: join(cwd, 'src'),
     skipLibCheck: true,
-    tsBuildInfoFile: join(cwd, '.cache', 'quisi-start.tsbuildinfo'),
+    tsBuildInfoFile: join(cwd, '.cache', 'quisido-start.tsbuildinfo'),
   };
 
   const jsx: server.protocol.JsxEmit | undefined = await createJsx();
