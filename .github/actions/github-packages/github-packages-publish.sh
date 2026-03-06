627e38c47334648919118ca739fd2d87497431ba
quisi.do <github@quisi.do>
2026-03-05 21:49:09 -0800

    remove CodeQL and Devskim

--- .github/actions/github-packages/github-packages-publish.sh
+++ .github/actions/github-packages/github-packages-publish.sh
@@ -1,4 +1,4 @@
 set -Eeuo pipefail;
 
 npm config set "//npm.pkg.github.com/:_authToken" $NPM_AUTH_TOKEN;
-npm publish --registry=https://npm.pkg.github.com;
+npm run publish --registry=https://npm.pkg.github.com;
