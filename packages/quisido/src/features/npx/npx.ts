5d46ce95129a7c724b18f4378da56215d03c28e4
quisi.do <github@quisi.do>
2026-05-24 22:20:52 -0700

    fix npm cache in CI

--- packages/quisido/src/features/npx/npx.ts
+++ packages/quisido/src/features/npx/npx.ts
@@ -1,5 +1,4 @@
 import execute, { type ExecutionResult } from '../../utils/execute.js';
-import getDisposableTempDir from '../../utils/get-disposable-temp-dir.js';
 import { NPX_COMMAND } from './npx-command.js';
 
 interface Options {
@@ -7,13 +6,6 @@ interface Options {
   readonly onStdErr?: ((data: string) => void) | undefined;
   readonly onStdOut?: ((data: string) => void) | undefined;
 }
-
-const mapEnvToNpmCacheEnv = async (
-  env: Partial<Record<string, string | undefined>> = {},
-): Promise<Partial<Record<string, string | undefined>>> => ({
-  NPM_CONFIG_CACHE: await getDisposableTempDir(),
-  ...env,
-});
 
 export default async function npx(
   options: Options,
@@ -29,14 +21,12 @@ export default async function npx(
   const [npxFile, ...npxArgs] = NPX_COMMAND;
 
   if (typeof options === 'string') {
-    return await execute(npxFile, [...npxArgs, options, ...args], {
-      env: await mapEnvToNpmCacheEnv(),
-    });
+    return await execute(npxFile, [...npxArgs, options, ...args]);
   }
 
   const { env, onStdErr, onStdOut } = options;
   return await execute(npxFile, [...npxArgs, ...args], {
-    env: await mapEnvToNpmCacheEnv(env),
+    env,
     onStdErr,
     onStdOut,
   });
