b25c54fcef1d6a45a0fc35078ff7dc1eb2bcfce4
quisi.do <github@quisi.do>
2026-06-12 00:44:30 -0700

    tweaks

--- packages/use-force-update/test/validate-defined.ts
+++
@@ -1,7 +0,0 @@
-export default function validateDefined<T>(value: T | undefined): T {
-  if (typeof value === 'undefined') {
-    throw new Error('Expected value to be defined.');
-  }
-
-  return value;
-}
