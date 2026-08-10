065893cb5028631b0d4e0e854ae40fc71d494dff
quisi.do <github@quisi.do>
2026-08-09 20:48:09 -0700

    remove old files

--- packages/ai/scripts/dev-test.sh
+++
@@ -1,6 +0,0 @@
-node ./dev-test.js | \
-curl -X POST https://localhost:378 \
-     -H "Authorization: Bearer TEST_PLACEHOLDER_ACCESS_TOKEN" \
-     -H "Content-Type: application/json" \
-     --insecure \
-     -d @-
